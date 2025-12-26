dd7082b9aca57f87fbaaba59b69084f6dd34dc73
Chupin Dmitrii <mr.dmitriych@gmail.com>
2025-12-26 17:15:49 +0500

    add notfound document page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,9 @@ export const routes: Routes = [
     path: 'reader/:documentId/view',
     component: Reader,
   },
+  /** @note В идеале надо редиректить на список документов, но на данном этапе такой компонент отсутствует */
+  {
+    path: '**',
+    redirectTo: 'reader/1/view',
+  },
 ];
