d8052574b3c289a9910d2fd578cbe4d85e2c8a77
Chupin Dmitrii <mr.dmitriych@gmail.com>
2025-12-26 16:00:27 +0500

    add touchevent in drag directive

--- src/app/directives/drag.ts
+++ src/app/directives/drag.ts
@@ -1,5 +1,14 @@
 import { Directive, ElementRef, inject, input, output } from '@angular/core';
-import { finalize, fromEvent, map, Subscription, switchMap, takeUntil, tap } from 'rxjs';
+import {
+  finalize,
+  fromEvent,
+  map,
+  Observable,
+  Subscription,
+  switchMap,
+  takeUntil,
+  tap,
+} from 'rxjs';
 
 /** Директива для перетаскивания */
 @Directive({
@@ -22,16 +31,19 @@ export class Drag {
 
     if (!element) return;
 
+    this.handleMouse(element);
+    this.handleTouch(element);
+  }
+
+  /** Обработка нажатия мыши */
+  private handleMouse(element: HTMLDivElement): void {
     const mousedown$ = fromEvent<PointerEvent>(element, 'mousedown');
     const mousemove$ = fromEvent<PointerEvent>(document, 'mousemove');
     const mouseup$ = fromEvent<PointerEvent>(document, 'mouseup');
 
     const drag$ = mousedown$.pipe(
       switchMap((start) => {
-        const startX = start.clientX;
-        const startY = start.clientY;
-        const initialLeft = parseInt(element.style.left) || 0;
-        const initialTop = parseInt(element.style.top) || 0;
+        const { initialLeft, initialTop, startX, startY } = this.getCoordinates(element, start);
 
         return mousemove$.pipe(
           map((move) => ({
@@ -45,10 +57,61 @@ export class Drag {
       }),
     );
 
+    this.addSubscription(drag$, element);
+  }
+
+  /** Обработка тача */
+  private handleTouch(element: HTMLDivElement): void {
+    const touchstart$ = fromEvent<TouchEvent>(element, 'touchstart');
+    const touchmove$ = fromEvent<TouchEvent>(document, 'touchmove');
+    const touchend$ = fromEvent<TouchEvent>(document, 'touchend');
+
+    const drag$ = touchstart$.pipe(
+      switchMap((start) => {
+        const { initialLeft, initialTop, startX, startY } = this.getCoordinates(element, start);
+
+        return touchmove$.pipe(
+          map((move) => ({
+            x: initialLeft + (move.touches[0].clientX - startX),
+            y: initialTop + (move.touches[0].clientY - startY),
+          })),
+          tap((move) => (this.coordinates = { x: move.x, y: move.y })),
+          takeUntil(touchend$),
+          finalize(() => this.handleDrag.emit(this.coordinates)),
+        );
+      }),
+    );
+
+    this.addSubscription(drag$, element);
+  }
+
+  /** Координаты элемента и pointer-а */
+  private getCoordinates(
+    element: HTMLDivElement,
+    event: TouchEvent | PointerEvent,
+  ): { initialLeft: number; initialTop: number; startX: number; startY: number } {
+    const initialLeft = parseInt(element.style.left) || 0;
+    const initialTop = parseInt(element.style.top) || 0;
+    const startX = event instanceof TouchEvent ? event.touches[0].clientX : event.clientX;
+    const startY = event instanceof TouchEvent ? event.touches[0].clientY : event.clientY;
+
+    return {
+      initialLeft,
+      initialTop,
+      startX,
+      startY,
+    };
+  }
+
+  /** Добавляем подписку на элемент */
+  private addSubscription(
+    drag$: Observable<{ x: number; y: number }>,
+    element: HTMLDivElement,
+  ): void {
     this.subscriptions.add(
-      drag$.subscribe((pos) => {
-        element.style.left = `${pos.x}px`;
-        element.style.top = `${pos.y}px`;
+      drag$.subscribe((position) => {
+        element.style.left = `${position.x}px`;
+        element.style.top = `${position.y}px`;
       }),
     );
   }
